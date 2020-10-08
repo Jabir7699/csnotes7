45fc1fb610c014c75315af587ab959e635e403bc
Jabir <47657789+Jabir7699@users.noreply.github.com>
2020-10-08 12:10:16 +0530

    Add files via upload

---
+++ js/notes.js
@@ -0,0 +1,11 @@
+function fun(){
+    //swal("Good job!", "Downloading..!", "success");
+    //swal("Hello world!");
+    swal({
+        title: "Good job!",
+        text: "You clicked the button!",
+        icon: "success",
+        button: "Aww yiss!",
+      });
+}
+
